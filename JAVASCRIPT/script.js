b4702709faa3323d34a121d2bd5dd990a00776fa
Geovani237 <94084031+Geovani237@users.noreply.github.com>
2022-08-17 14:18:19 -0300

    Testes com a operação de Soma

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,15 @@
+function calc(num1,num2){
+    /*
+  let n1, n2, n3, oper
+
+    n1 = document.querySelector("#num1").value;
+    n1 = parseFloat(n1)
+    n2 = document.querySelector("#num2").value;
+    n2 = parseFloat(n2)
+
+    oper = document.querySelector("#operacao").value;
+    n3 = parseFloat(n1) + parseFloat(n2);
+
+    document.querySelector("#res").innerHTML ="Resultado:" + n3
+    */
+}
