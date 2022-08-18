7b99d2ffc5a19e7d9c2092d9faa51412c41c14e2
Geovani carlos de souza <94084031+Geovani237@users.noreply.github.com>
2022-08-17 22:16:50 -0300

    Javascript básico, com os operadores

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,5 +1,5 @@
 function calc(num1,num2){
-    /*
+    
   let n1, n2, n3, oper
 
     n1 = document.querySelector("#num1").value;
@@ -8,8 +8,24 @@ function calc(num1,num2){
     n2 = parseFloat(n2)
 
     oper = document.querySelector("#operacao").value;
-    n3 = parseFloat(n1) + parseFloat(n2);
+   /* if(oper == "selecione"){
 
-    document.querySelector("#res").innerHTML ="Resultado:" + n3
-    */
+   }
+   */
+    if(oper == "soma") {
+      n3 = parseFloat(n1) + parseFloat(n2);
+    }
+    if (oper == "subtracao"){
+      n3 = parseFloat(n1) - parseFloat(n2);
+    }
+    if (oper == "divisao"){
+      n3 = parseFloat(n1) / parseFloat(n2);
+    }
+    if(oper == "multiplicacao"){
+      n3 = parseFloat(n1) * parseFloat(n2);
+    }
+    
+
+    document.querySelector("#res").innerHTML ="Resultado: " + n3
+    
 }
