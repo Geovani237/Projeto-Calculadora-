3f425e5c0f14da0ad3550fd8858277219049f69c
Geovani237 <94084031+Geovani237@users.noreply.github.com>
2022-08-19 14:36:07 -0300

    refinamento do javascript
    começo dos números verdes e vermelho, como proposto...

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -8,10 +8,12 @@ function calc(num1,num2){
     n2 = parseFloat(n2)
 
     oper = document.querySelector("#operacao").value;
-   /* if(oper == "selecione"){
+    // posso fazer um enquanto para as seleção para ficar um pouco menor!!
 
-   }
-   */
+    //----OPERAÇÃO-----
+    if(oper == "selecione"){
+     alert("Nenhuma operação selecionada!!!");
+    }
     if(oper == "soma") {
       n3 = parseFloat(n1) + parseFloat(n2);
     }
@@ -19,13 +21,49 @@ function calc(num1,num2){
       n3 = parseFloat(n1) - parseFloat(n2);
     }
     if (oper == "divisao"){
-      n3 = parseFloat(n1) / parseFloat(n2);
+      
+       if(num1 == 0 || num2 == 0){
+        alert("Não é possivel dividir por ZERO!")
+       }
+       n3 = parseFloat(n1) / parseFloat(n2);
+      }
+      if(oper == "multiplicacao"){
+        n3 = parseFloat(n1) * parseFloat(n2);
+      }
+    
+     //----NÚMERO NÃO SELECIONADO-----
+    if(num1 == "" && num2 ==""){
+      alert("Os campos de número estão em branco, Digite para Continuar!");
+      }
+      else if(num1 == ""){
+        alert("Digite o 1 número para continuar");
+      }
+      else if(num2 == ""){
+        alert("Digite o 2 número para continuar");
+      }
+      //-----VERDE E VERMELHO NÚMEROS------
+      if(n3 > 0){
+        document.getElementsByTagName("h2").style.color = 'green';
+        document.querySelector("#res").innerHTML ="Resultado: " + n3
+      }
+    //document.querySelector("#res").innerHTML ="Resultado: " + n3
+  
     }
+    /*
     if(oper == "multiplicacao"){
       n3 = parseFloat(n1) * parseFloat(n2);
     }
+  
+    //----NÚMERO NÃO SELECIONADO-----
+    if(num1 == "" && num2 ==""){
+      alert("Os campos de número estão em branco, Digite para Continuar!");
+    }
+    else if(num1 == ""){
+      alert("Digite o 1 número para continuar");
+    }
+    else if(num2 == ""){
+      alert("Digite o 2 número para continuar");
+    }
     
-
     document.querySelector("#res").innerHTML ="Resultado: " + n3
-    
-}
+    */
